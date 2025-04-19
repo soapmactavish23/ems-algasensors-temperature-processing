9f1e5ea677b9693accf4b72554cea7244e1e3a26
Henrick <ricknogueira1231@gmail.com>
2025-04-18 23:31:38 -0300

    10.11. Implementando microsserviço de Temperature Processing

---
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -0,0 +1,43 @@
+package com.algaworks.algasensors.temperature.processing.api.controller;
+
+import com.algaworks.algasensors.temperature.processing.IdGenerator;
+import com.algaworks.algasensors.temperature.processing.api.model.TemperatureLogOutput;
+import io.hypersistence.tsid.TSID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.OffsetDateTime;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/sensors/{sensorId}/temperatures/data")
+public class TemperatureProcessingController {
+
+    @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
+    public void data(@PathVariable TSID sensorId, @RequestBody String input) {
+        if(input == null || input.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        Double temperature;
+
+        try {
+            temperature = Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        TemperatureLogOutput logOutput = TemperatureLogOutput.builder()
+                .id(IdGenerator.generateTimeBasedUUID())
+                .sensorId(sensorId)
+                .value(temperature)
+                .registeredAt(OffsetDateTime.now())
+                .build();
+
+        log.info(logOutput.toString());
+    }
+
+}
